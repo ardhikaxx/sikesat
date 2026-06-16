9df37bf2c8f9337c8f5cea5d60acf8b9daf16314
ardhikaxx <ardhikayanuar58@gmail.com>
2026-06-16 18:43:38 +0700

    feat: penambahan fitur resources/views/stok-gudang/show.blade.php

---
+++ resources/views/stok-gudang/show.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+@section('title', 'Detail StokGudang - SIKESAT')
+@section('content')
+<div class="page-header">
+    <div class="page-header__left">
+        <h1 class="page-header__title">Detail StokGudang</h1>
+        <a href="{{ route('stok-gudang.index') }}" class="text-decoration-none">&larr; Kembali</a>
+    </div>
+</div>
+<div class="card border-0 shadow-sm rounded-3">
+    <div class="card-body p-4">
+        <table class="table table-bordered">
+            <tr><th width="25%">Id</th><td>{{ $data->id }}</td></tr>
+            <tr><th width="25%">Obat Alkes Id</th><td>{{ $data->obat_alkes_id }}</td></tr>
+            <tr><th width="25%">No Batch</th><td>{{ $data->no_batch }}</td></tr>
+            <tr><th width="25%">Tanggal Kedaluwarsa</th><td>{{ $data->tanggal_kedaluwarsa }}</td></tr>
+            <tr><th width="25%">Jumlah Masuk</th><td>{{ $data->jumlah_masuk }}</td></tr>
+            <tr><th width="25%">Jumlah Keluar</th><td>{{ $data->jumlah_keluar }}</td></tr>
+            <tr><th width="25%">Stok Tersedia</th><td>{{ $data->stok_tersedia }}</td></tr>
+            <tr><th width="25%">Harga Perolehan</th><td>{{ $data->harga_perolehan }}</td></tr>
+            <tr><th width="25%">Pengeluaran Id</th><td>{{ $data->pengeluaran_id }}</td></tr>
+            <tr><th width="25%">Created At</th><td>{{ $data->created_at }}</td></tr>
+            <tr><th width="25%">Updated At</th><td>{{ $data->updated_at }}</td></tr>
+        </table>
+    </div>
+</div>
+@endsection
