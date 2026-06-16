b84ae3e615762d50497fa6fd7d3ba3093790d45b
ardhikaxx <ardhikayanuar58@gmail.com>
2026-06-16 18:43:06 +0700

    feat: penambahan fitur app/Http/Controllers/PegawaiController.php

--- app/Http/Controllers/PegawaiController.php
+++ app/Http/Controllers/PegawaiController.php
@@ -9,14 +9,43 @@ class PegawaiController extends Controller
 {
     public function index()
     {
-        $pegawais = Pegawai::with('unit')->get();
-        return view('pegawai.index', compact('pegawais'));
+        $data = Pegawai::latest()->get();
+        return view('pegawai.index', compact('data'));
+    }
+
+    public function create()
+    {
+        return view('pegawai.create');
+    }
+
+    public function store(Request $request)
+    {
+        Pegawai::create($request->except('_token'));
+        return redirect()->route('pegawai.index')->with('sukses', 'Data berhasil ditambahkan');
+    }
+
+    public function show($id)
+    {
+        $data = Pegawai::findOrFail($id);
+        return view('pegawai.show', compact('data'));
+    }
+
+    public function edit($id)
+    {
+        $data = Pegawai::findOrFail($id);
+        return view('pegawai.edit', compact('data'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $data = Pegawai::findOrFail($id);
+        $data->update($request->except(['_token', '_method']));
+        return redirect()->route('pegawai.index')->with('sukses', 'Data berhasil diupdate');
     }
 
     public function destroy($id)
     {
-        $pegawai = Pegawai::findOrFail($id);
-        $pegawai->delete();
-        return response()->json(['success' => true, 'message' => 'Data pegawai berhasil dihapus.']);
+        Pegawai::destroy($id);
+        return redirect()->route('pegawai.index')->with('sukses', 'Data berhasil dihapus');
     }
 }
