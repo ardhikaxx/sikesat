49a77d4bbf3d4a7c9c2853d0d78bb49e2e4b2c45
ardhikaxx <ardhikayanuar58@gmail.com>
2026-06-16 18:43:26 +0700

    feat: penambahan fitur resources/views/kunjungan-pasien/show.blade.php

---
+++ resources/views/kunjungan-pasien/show.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+@section('title', 'Detail KunjunganPasien - SIKESAT')
+@section('content')
+<div class="page-header">
+    <div class="page-header__left">
+        <h1 class="page-header__title">Detail KunjunganPasien</h1>
+        <a href="{{ route('kunjungan-pasien.index') }}" class="text-decoration-none">&larr; Kembali</a>
+    </div>
+</div>
+<div class="card border-0 shadow-sm rounded-3">
+    <div class="card-body p-4">
+        <table class="table table-bordered">
+            <tr><th width="25%">Id</th><td>{{ $data->id }}</td></tr>
+            <tr><th width="25%">Unit Id</th><td>{{ $data->unit_id }}</td></tr>
+            <tr><th width="25%">Tanggal</th><td>{{ $data->tanggal }}</td></tr>
+            <tr><th width="25%">Jumlah Kunjungan Umum</th><td>{{ $data->jumlah_kunjungan_umum }}</td></tr>
+            <tr><th width="25%">Jumlah Kunjungan Bpjs</th><td>{{ $data->jumlah_kunjungan_bpjs }}</td></tr>
+            <tr><th width="25%">Jumlah Kunjungan Gratis</th><td>{{ $data->jumlah_kunjungan_gratis }}</td></tr>
+            <tr><th width="25%">Total Kunjungan</th><td>{{ $data->total_kunjungan }}</td></tr>
+            <tr><th width="25%">Rata Waktu Tunggu</th><td>{{ $data->rata_waktu_tunggu }}</td></tr>
+            <tr><th width="25%">Input Oleh</th><td>{{ $data->input_oleh }}</td></tr>
+            <tr><th width="25%">Created At</th><td>{{ $data->created_at }}</td></tr>
+            <tr><th width="25%">Updated At</th><td>{{ $data->updated_at }}</td></tr>
+        </table>
+    </div>
+</div>
+@endsection
