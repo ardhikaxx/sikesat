e1f851e38ed7a7e966cc000a55143bd1b5a623e7
ardhikaxx <ardhikayanuar58@gmail.com>
2026-06-16 18:43:24 +0700

    feat: penambahan fitur resources/views/jurnal-penyesuaian/show.blade.php

---
+++ resources/views/jurnal-penyesuaian/show.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+@section('title', 'Detail JurnalPenyesuaian - SIKESAT')
+@section('content')
+<div class="page-header">
+    <div class="page-header__left">
+        <h1 class="page-header__title">Detail JurnalPenyesuaian</h1>
+        <a href="{{ route('jurnal-penyesuaian.index') }}" class="text-decoration-none">&larr; Kembali</a>
+    </div>
+</div>
+<div class="card border-0 shadow-sm rounded-3">
+    <div class="card-body p-4">
+        <table class="table table-bordered">
+            <tr><th width="25%">Id</th><td>{{ $data->id }}</td></tr>
+            <tr><th width="25%">Tahun Anggaran Id</th><td>{{ $data->tahun_anggaran_id }}</td></tr>
+            <tr><th width="25%">Tanggal</th><td>{{ $data->tanggal }}</td></tr>
+            <tr><th width="25%">Jenis</th><td>{{ $data->jenis }}</td></tr>
+            <tr><th width="25%">Keterangan</th><td>{{ $data->keterangan }}</td></tr>
+            <tr><th width="25%">Status</th><td>{{ $data->status }}</td></tr>
+            <tr><th width="25%">Disetujui Oleh</th><td>{{ $data->disetujui_oleh }}</td></tr>
+            <tr><th width="25%">Created At</th><td>{{ $data->created_at }}</td></tr>
+            <tr><th width="25%">Updated At</th><td>{{ $data->updated_at }}</td></tr>
+        </table>
+    </div>
+</div>
+@endsection
