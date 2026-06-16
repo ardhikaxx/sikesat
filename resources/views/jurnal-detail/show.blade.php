fba1c4977ddb94a73f7c795f16edc86e25d134d8
ardhikaxx <ardhikayanuar58@gmail.com>
2026-06-16 18:43:24 +0700

    feat: penambahan fitur resources/views/jurnal-detail/show.blade.php

---
+++ resources/views/jurnal-detail/show.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+@section('title', 'Detail JurnalDetail - SIKESAT')
+@section('content')
+<div class="page-header">
+    <div class="page-header__left">
+        <h1 class="page-header__title">Detail JurnalDetail</h1>
+        <a href="{{ route('jurnal-detail.index') }}" class="text-decoration-none">&larr; Kembali</a>
+    </div>
+</div>
+<div class="card border-0 shadow-sm rounded-3">
+    <div class="card-body p-4">
+        <table class="table table-bordered">
+            <tr><th width="25%">Id</th><td>{{ $data->id }}</td></tr>
+            <tr><th width="25%">Jurnal Id</th><td>{{ $data->jurnal_id }}</td></tr>
+            <tr><th width="25%">Akun Id</th><td>{{ $data->akun_id }}</td></tr>
+            <tr><th width="25%">Posisi</th><td>{{ $data->posisi }}</td></tr>
+            <tr><th width="25%">Jumlah</th><td>{{ $data->jumlah }}</td></tr>
+            <tr><th width="25%">Keterangan</th><td>{{ $data->keterangan }}</td></tr>
+            <tr><th width="25%">Created At</th><td>{{ $data->created_at }}</td></tr>
+            <tr><th width="25%">Updated At</th><td>{{ $data->updated_at }}</td></tr>
+        </table>
+    </div>
+</div>
+@endsection
