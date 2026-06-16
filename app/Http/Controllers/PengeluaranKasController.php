082e31d0a51d592df22bdbf2e26355a1ba13c986
ardhikaxx <ardhikayanuar58@gmail.com>
2026-06-16 18:43:06 +0700

    feat: penambahan fitur app/Http/Controllers/PengeluaranKasController.php

--- app/Http/Controllers/PengeluaranKasController.php
+++ app/Http/Controllers/PengeluaranKasController.php
@@ -9,7 +9,43 @@ class PengeluaranKasController extends Controller
 {
     public function index()
     {
-        $pengeluarans = PengeluaranKas::with(['sumber_dana'])->orderBy('tanggal', 'desc')->get();
-        return view('pengeluaran.index', compact('pengeluarans'));
+        $data = PengeluaranKas::latest()->get();
+        return view('pengeluaran-kas.index', compact('data'));
+    }
+
+    public function create()
+    {
+        return view('pengeluaran-kas.create');
+    }
+
+    public function store(Request $request)
+    {
+        PengeluaranKas::create($request->except('_token'));
+        return redirect()->route('pengeluaran-kas.index')->with('sukses', 'Data berhasil ditambahkan');
+    }
+
+    public function show($id)
+    {
+        $data = PengeluaranKas::findOrFail($id);
+        return view('pengeluaran-kas.show', compact('data'));
+    }
+
+    public function edit($id)
+    {
+        $data = PengeluaranKas::findOrFail($id);
+        return view('pengeluaran-kas.edit', compact('data'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $data = PengeluaranKas::findOrFail($id);
+        $data->update($request->except(['_token', '_method']));
+        return redirect()->route('pengeluaran-kas.index')->with('sukses', 'Data berhasil diupdate');
+    }
+
+    public function destroy($id)
+    {
+        PengeluaranKas::destroy($id);
+        return redirect()->route('pengeluaran-kas.index')->with('sukses', 'Data berhasil dihapus');
     }
 }
