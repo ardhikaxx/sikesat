571d310056c53a9280bb22040086f00c8da86a55
ardhikaxx <ardhikayanuar58@gmail.com>
2026-06-16 18:43:36 +0700

    feat: penambahan fitur resources/views/rka-rincian/show.blade.php

---
+++ resources/views/rka-rincian/show.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+@section('title', 'Detail RkaRincian - SIKESAT')
+@section('content')
+<div class="page-header">
+    <div class="page-header__left">
+        <h1 class="page-header__title">Detail RkaRincian</h1>
+        <a href="{{ route('rka-rincian.index') }}" class="text-decoration-none">&larr; Kembali</a>
+    </div>
+</div>
+<div class="card border-0 shadow-sm rounded-3">
+    <div class="card-body p-4">
+        <table class="table table-bordered">
+            <tr><th width="25%">Id</th><td>{{ $data->id }}</td></tr>
+            <tr><th width="25%">Rka Id</th><td>{{ $data->rka_id }}</td></tr>
+            <tr><th width="25%">Akun Id</th><td>{{ $data->akun_id }}</td></tr>
+            <tr><th width="25%">Uraian</th><td>{{ $data->uraian }}</td></tr>
+            <tr><th width="25%">Volume</th><td>{{ $data->volume }}</td></tr>
+            <tr><th width="25%">Satuan</th><td>{{ $data->satuan }}</td></tr>
+            <tr><th width="25%">Harga Satuan</th><td>{{ $data->harga_satuan }}</td></tr>
+            <tr><th width="25%">Total</th><td>{{ $data->total }}</td></tr>
+            <tr><th width="25%">Created At</th><td>{{ $data->created_at }}</td></tr>
+            <tr><th width="25%">Updated At</th><td>{{ $data->updated_at }}</td></tr>
+        </table>
+    </div>
+</div>
+@endsection
