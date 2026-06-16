faf57801192d900c743fb45a699334a4444da00c
ardhikaxx <ardhikayanuar58@gmail.com>
2026-06-16 18:43:19 +0700

    feat: penambahan fitur resources/views/akun-akuntansi/show.blade.php

---
+++ resources/views/akun-akuntansi/show.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+@section('title', 'Detail AkunAkuntansi - SIKESAT')
+@section('content')
+<div class="page-header">
+    <div class="page-header__left">
+        <h1 class="page-header__title">Detail AkunAkuntansi</h1>
+        <a href="{{ route('akun-akuntansi.index') }}" class="text-decoration-none">&larr; Kembali</a>
+    </div>
+</div>
+<div class="card border-0 shadow-sm rounded-3">
+    <div class="card-body p-4">
+        <table class="table table-bordered">
+            <tr><th width="25%">Id</th><td>{{ $data->id }}</td></tr>
+            <tr><th width="25%">Kode Akun</th><td>{{ $data->kode_akun }}</td></tr>
+            <tr><th width="25%">Nama Akun</th><td>{{ $data->nama_akun }}</td></tr>
+            <tr><th width="25%">Jenis Akun</th><td>{{ $data->jenis_akun }}</td></tr>
+            <tr><th width="25%">Kelompok Akun</th><td>{{ $data->kelompok_akun }}</td></tr>
+            <tr><th width="25%">Akun Induk Id</th><td>{{ $data->akun_induk_id }}</td></tr>
+            <tr><th width="25%">Level</th><td>{{ $data->level }}</td></tr>
+            <tr><th width="25%">Saldo Normal</th><td>{{ $data->saldo_normal }}</td></tr>
+            <tr><th width="25%">Is Aktif</th><td>{{ $data->is_aktif }}</td></tr>
+            <tr><th width="25%">Keterangan</th><td>{{ $data->keterangan }}</td></tr>
+            <tr><th width="25%">Created At</th><td>{{ $data->created_at }}</td></tr>
+            <tr><th width="25%">Updated At</th><td>{{ $data->updated_at }}</td></tr>
+        </table>
+    </div>
+</div>
+@endsection
