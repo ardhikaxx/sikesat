cc41e1d17f8dd1b3009c5463946a6d6d9d74eea1
ardhikaxx <ardhikayanuar58@gmail.com>
2026-06-16 18:43:22 +0700

    feat: penambahan fitur resources/views/indikator-mutu/show.blade.php

---
+++ resources/views/indikator-mutu/show.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+@section('title', 'Detail IndikatorMutu - SIKESAT')
+@section('content')
+<div class="page-header">
+    <div class="page-header__left">
+        <h1 class="page-header__title">Detail IndikatorMutu</h1>
+        <a href="{{ route('indikator-mutu.index') }}" class="text-decoration-none">&larr; Kembali</a>
+    </div>
+</div>
+<div class="card border-0 shadow-sm rounded-3">
+    <div class="card-body p-4">
+        <table class="table table-bordered">
+            <tr><th width="25%">Id</th><td>{{ $data->id }}</td></tr>
+            <tr><th width="25%">Kode</th><td>{{ $data->kode }}</td></tr>
+            <tr><th width="25%">Nama</th><td>{{ $data->nama }}</td></tr>
+            <tr><th width="25%">Satuan</th><td>{{ $data->satuan }}</td></tr>
+            <tr><th width="25%">Jenis</th><td>{{ $data->jenis }}</td></tr>
+            <tr><th width="25%">Target Nilai</th><td>{{ $data->target_nilai }}</td></tr>
+            <tr><th width="25%">Arah Target</th><td>{{ $data->arah_target }}</td></tr>
+            <tr><th width="25%">Unit Id</th><td>{{ $data->unit_id }}</td></tr>
+            <tr><th width="25%">Is Aktif</th><td>{{ $data->is_aktif }}</td></tr>
+            <tr><th width="25%">Keterangan</th><td>{{ $data->keterangan }}</td></tr>
+            <tr><th width="25%">Created At</th><td>{{ $data->created_at }}</td></tr>
+            <tr><th width="25%">Updated At</th><td>{{ $data->updated_at }}</td></tr>
+        </table>
+    </div>
+</div>
+@endsection
