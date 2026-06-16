dbbc98c9a8bfcb5d3acf2eae7ff9d9dd567d65cf
ardhikaxx <ardhikayanuar58@gmail.com>
2026-06-16 18:43:30 +0700

    feat: penambahan fitur resources/views/pemeliharaan-aset/show.blade.php

---
+++ resources/views/pemeliharaan-aset/show.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+@section('title', 'Detail PemeliharaanAset - SIKESAT')
+@section('content')
+<div class="page-header">
+    <div class="page-header__left">
+        <h1 class="page-header__title">Detail PemeliharaanAset</h1>
+        <a href="{{ route('pemeliharaan-aset.index') }}" class="text-decoration-none">&larr; Kembali</a>
+    </div>
+</div>
+<div class="card border-0 shadow-sm rounded-3">
+    <div class="card-body p-4">
+        <table class="table table-bordered">
+            <tr><th width="25%">Id</th><td>{{ $data->id }}</td></tr>
+            <tr><th width="25%">Aset Id</th><td>{{ $data->aset_id }}</td></tr>
+            <tr><th width="25%">Tanggal</th><td>{{ $data->tanggal }}</td></tr>
+            <tr><th width="25%">Jenis Pemeliharaan</th><td>{{ $data->jenis_pemeliharaan }}</td></tr>
+            <tr><th width="25%">Biaya</th><td>{{ $data->biaya }}</td></tr>
+            <tr><th width="25%">Pengeluaran Id</th><td>{{ $data->pengeluaran_id }}</td></tr>
+            <tr><th width="25%">Kondisi Sebelum</th><td>{{ $data->kondisi_sebelum }}</td></tr>
+            <tr><th width="25%">Kondisi Sesudah</th><td>{{ $data->kondisi_sesudah }}</td></tr>
+            <tr><th width="25%">Dilakukan Oleh</th><td>{{ $data->dilakukan_oleh }}</td></tr>
+            <tr><th width="25%">Keterangan</th><td>{{ $data->keterangan }}</td></tr>
+            <tr><th width="25%">Created At</th><td>{{ $data->created_at }}</td></tr>
+            <tr><th width="25%">Updated At</th><td>{{ $data->updated_at }}</td></tr>
+        </table>
+    </div>
+</div>
+@endsection
