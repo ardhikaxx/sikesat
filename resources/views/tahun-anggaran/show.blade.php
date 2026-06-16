66cde3905bc0e9f45099b7d9bc703064f5c4ad9e
ardhikaxx <ardhikayanuar58@gmail.com>
2026-06-16 18:43:40 +0700

    feat: penambahan fitur resources/views/tahun-anggaran/show.blade.php

---
+++ resources/views/tahun-anggaran/show.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+@section('title', 'Detail TahunAnggaran - SIKESAT')
+@section('content')
+<div class="page-header">
+    <div class="page-header__left">
+        <h1 class="page-header__title">Detail TahunAnggaran</h1>
+        <a href="{{ route('tahun-anggaran.index') }}" class="text-decoration-none">&larr; Kembali</a>
+    </div>
+</div>
+<div class="card border-0 shadow-sm rounded-3">
+    <div class="card-body p-4">
+        <table class="table table-bordered">
+            <tr><th width="25%">Id</th><td>{{ $data->id }}</td></tr>
+            <tr><th width="25%">Tahun</th><td>{{ $data->tahun }}</td></tr>
+            <tr><th width="25%">Status</th><td>{{ $data->status }}</td></tr>
+            <tr><th width="25%">Tanggal Mulai</th><td>{{ $data->tanggal_mulai }}</td></tr>
+            <tr><th width="25%">Tanggal Selesai</th><td>{{ $data->tanggal_selesai }}</td></tr>
+            <tr><th width="25%">Catatan</th><td>{{ $data->catatan }}</td></tr>
+            <tr><th width="25%">Created At</th><td>{{ $data->created_at }}</td></tr>
+            <tr><th width="25%">Updated At</th><td>{{ $data->updated_at }}</td></tr>
+        </table>
+    </div>
+</div>
+@endsection
