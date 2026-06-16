6b9185ab89da7fcd55919a8c0de8649643f61056
ardhikaxx <ardhikayanuar58@gmail.com>
2026-06-16 18:43:22 +0700

    feat: penambahan fitur resources/views/indikator-mutu/index.blade.php

---
+++ resources/views/indikator-mutu/index.blade.php
@@ -0,0 +1,49 @@
+@extends('layouts.app')
+@section('title', 'Data IndikatorMutu - SIKESAT')
+@section('content')
+<div class="page-header">
+    <div class="page-header__left">
+        <h1 class="page-header__title"><i class="fas fa-table"></i> Data IndikatorMutu</h1>
+    </div>
+    <div class="page-header__actions">
+        <a href="{{ route('indikator-mutu.create') }}" class="btn-primary-sikesat"><i class="fas fa-plus"></i> Tambah Data</a>
+    </div>
+</div>
+<div class="card border-0 shadow-sm rounded-3">
+    <div class="card-body p-4">
+        <div class="table-responsive">
+            <table class="table sikesat-table">
+                <thead>
+                    <tr>
+                        <th>Kode</th>
+                        <th>Nama</th>
+                        <th>Satuan</th>
+                        <th>Jenis</th>
+                        <th class="text-center">Aksi</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($data as $item)
+                    <tr>
+                        <td>{{ $item->kode }}</td>
+                        <td>{{ $item->nama }}</td>
+                        <td>{{ $item->satuan }}</td>
+                        <td>{{ $item->jenis }}</td>
+                        <td class="text-center">
+                            <div class="d-flex justify-content-center gap-1">
+                                <a href="{{ route('indikator-mutu.show', $item->id) }}" class="btn-action btn-action-view"><i class="fas fa-eye"></i></a>
+                                <a href="{{ route('indikator-mutu.edit', $item->id) }}" class="btn-action btn-action-edit"><i class="fas fa-pen"></i></a>
+                                <form action="{{ route('indikator-mutu.destroy', $item->id) }}" method="POST" class="d-inline" onsubmit="return confirm('Yakin hapus data ini?')">
+                                    @csrf @method('DELETE')
+                                    <button class="btn-action btn-action-delete"><i class="fas fa-trash"></i></button>
+                                </form>
+                            </div>
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+@endsection
