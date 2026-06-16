892238f3abe85e57a659c21352fbf6b01a63b8f1
ardhikaxx <ardhikayanuar58@gmail.com>
2026-06-16 18:43:05 +0700

    feat: penambahan fitur app/Http/Controllers/AkunAkuntansiController.php

--- app/Http/Controllers/AkunAkuntansiController.php
+++ app/Http/Controllers/AkunAkuntansiController.php
@@ -9,7 +9,43 @@ class AkunAkuntansiController extends Controller
 {
     public function index()
     {
-        $akuns = AkunAkuntansi::orderBy('kode_akun')->get();
-        return view('akun-akuntansi.index', compact('akuns'));
+        $data = AkunAkuntansi::latest()->get();
+        return view('akun-akuntansi.index', compact('data'));
+    }
+
+    public function create()
+    {
+        return view('akun-akuntansi.create');
+    }
+
+    public function store(Request $request)
+    {
+        AkunAkuntansi::create($request->except('_token'));
+        return redirect()->route('akun-akuntansi.index')->with('sukses', 'Data berhasil ditambahkan');
+    }
+
+    public function show($id)
+    {
+        $data = AkunAkuntansi::findOrFail($id);
+        return view('akun-akuntansi.show', compact('data'));
+    }
+
+    public function edit($id)
+    {
+        $data = AkunAkuntansi::findOrFail($id);
+        return view('akun-akuntansi.edit', compact('data'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $data = AkunAkuntansi::findOrFail($id);
+        $data->update($request->except(['_token', '_method']));
+        return redirect()->route('akun-akuntansi.index')->with('sukses', 'Data berhasil diupdate');
+    }
+
+    public function destroy($id)
+    {
+        AkunAkuntansi::destroy($id);
+        return redirect()->route('akun-akuntansi.index')->with('sukses', 'Data berhasil dihapus');
     }
 }
