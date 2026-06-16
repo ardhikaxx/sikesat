8f219cbf130fa57cd19cd82fd1e7f898f4fc5a7c
ardhikaxx <ardhikayanuar58@gmail.com>
2026-06-16 18:43:06 +0700

    feat: penambahan fitur app/Http/Controllers/JurnalUmumController.php

--- app/Http/Controllers/JurnalUmumController.php
+++ app/Http/Controllers/JurnalUmumController.php
@@ -9,7 +9,43 @@ class JurnalUmumController extends Controller
 {
     public function index()
     {
-        $jurnals = JurnalUmum::orderBy('tanggal', 'desc')->get();
-        return view('jurnal.index', compact('jurnals'));
+        $data = JurnalUmum::latest()->get();
+        return view('jurnal-umum.index', compact('data'));
+    }
+
+    public function create()
+    {
+        return view('jurnal-umum.create');
+    }
+
+    public function store(Request $request)
+    {
+        JurnalUmum::create($request->except('_token'));
+        return redirect()->route('jurnal-umum.index')->with('sukses', 'Data berhasil ditambahkan');
+    }
+
+    public function show($id)
+    {
+        $data = JurnalUmum::findOrFail($id);
+        return view('jurnal-umum.show', compact('data'));
+    }
+
+    public function edit($id)
+    {
+        $data = JurnalUmum::findOrFail($id);
+        return view('jurnal-umum.edit', compact('data'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $data = JurnalUmum::findOrFail($id);
+        $data->update($request->except(['_token', '_method']));
+        return redirect()->route('jurnal-umum.index')->with('sukses', 'Data berhasil diupdate');
+    }
+
+    public function destroy($id)
+    {
+        JurnalUmum::destroy($id);
+        return redirect()->route('jurnal-umum.index')->with('sukses', 'Data berhasil dihapus');
     }
 }
