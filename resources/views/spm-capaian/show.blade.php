ba5f602a0712ceec3767f678c456989ed0ed5d90
ardhikaxx <ardhikayanuar58@gmail.com>
2026-06-16 18:43:37 +0700

    feat: penambahan fitur resources/views/spm-capaian/show.blade.php

---
+++ resources/views/spm-capaian/show.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+@section('title', 'Detail SpmCapaian - SIKESAT')
+@section('content')
+<div class="page-header">
+    <div class="page-header__left">
+        <h1 class="page-header__title">Detail SpmCapaian</h1>
+        <a href="{{ route('spm-capaian.index') }}" class="text-decoration-none">&larr; Kembali</a>
+    </div>
+</div>
+<div class="card border-0 shadow-sm rounded-3">
+    <div class="card-body p-4">
+        <table class="table table-bordered">
+            <tr><th width="25%">Id</th><td>{{ $data->id }}</td></tr>
+            <tr><th width="25%">Spm Indikator Id</th><td>{{ $data->spm_indikator_id }}</td></tr>
+            <tr><th width="25%">Periode Bulan</th><td>{{ $data->periode_bulan }}</td></tr>
+            <tr><th width="25%">Periode Tahun</th><td>{{ $data->periode_tahun }}</td></tr>
+            <tr><th width="25%">Nilai Capaian</th><td>{{ $data->nilai_capaian }}</td></tr>
+            <tr><th width="25%">Keterangan</th><td>{{ $data->keterangan }}</td></tr>
+            <tr><th width="25%">Created At</th><td>{{ $data->created_at }}</td></tr>
+            <tr><th width="25%">Updated At</th><td>{{ $data->updated_at }}</td></tr>
+        </table>
+    </div>
+</div>
+@endsection
