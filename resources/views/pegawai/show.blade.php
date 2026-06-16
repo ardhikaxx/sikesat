a3e5cb31823bacc0d291a3263f894c624c166751
ardhikaxx <ardhikayanuar58@gmail.com>
2026-06-16 18:43:29 +0700

    feat: penambahan fitur resources/views/pegawai/show.blade.php

---
+++ resources/views/pegawai/show.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app')
+@section('title', 'Detail Pegawai - SIKESAT')
+@section('content')
+<div class="page-header">
+    <div class="page-header__left">
+        <h1 class="page-header__title">Detail Pegawai</h1>
+        <a href="{{ route('pegawai.index') }}" class="text-decoration-none">&larr; Kembali</a>
+    </div>
+</div>
+<div class="card border-0 shadow-sm rounded-3">
+    <div class="card-body p-4">
+        <table class="table table-bordered">
+            <tr><th width="25%">Id</th><td>{{ $data->id }}</td></tr>
+            <tr><th width="25%">User Id</th><td>{{ $data->user_id }}</td></tr>
+            <tr><th width="25%">Nip</th><td>{{ $data->nip }}</td></tr>
+            <tr><th width="25%">Nama</th><td>{{ $data->nama }}</td></tr>
+            <tr><th width="25%">Jenis Kelamin</th><td>{{ $data->jenis_kelamin }}</td></tr>
+            <tr><th width="25%">Tempat Lahir</th><td>{{ $data->tempat_lahir }}</td></tr>
+            <tr><th width="25%">Tanggal Lahir</th><td>{{ $data->tanggal_lahir }}</td></tr>
+            <tr><th width="25%">Alamat</th><td>{{ $data->alamat }}</td></tr>
+            <tr><th width="25%">No Telepon</th><td>{{ $data->no_telepon }}</td></tr>
+            <tr><th width="25%">Jabatan</th><td>{{ $data->jabatan }}</td></tr>
+            <tr><th width="25%">Golongan</th><td>{{ $data->golongan }}</td></tr>
+            <tr><th width="25%">Jenis Pegawai</th><td>{{ $data->jenis_pegawai }}</td></tr>
+            <tr><th width="25%">Unit Id</th><td>{{ $data->unit_id }}</td></tr>
+            <tr><th width="25%">Tanggal Masuk</th><td>{{ $data->tanggal_masuk }}</td></tr>
+            <tr><th width="25%">Status Aktif</th><td>{{ $data->status_aktif }}</td></tr>
+            <tr><th width="25%">Created At</th><td>{{ $data->created_at }}</td></tr>
+            <tr><th width="25%">Updated At</th><td>{{ $data->updated_at }}</td></tr>
+            <tr><th width="25%">Deleted At</th><td>{{ $data->deleted_at }}</td></tr>
+        </table>
+    </div>
+</div>
+@endsection
