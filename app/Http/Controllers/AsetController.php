261d34b108c008c1805f5f6d8da43bbb0284b66a
ardhikaxx <ardhikayanuar58@gmail.com>
2026-06-16 18:43:05 +0700

    feat: penambahan fitur app/Http/Controllers/AsetController.php

--- app/Http/Controllers/AsetController.php
+++ app/Http/Controllers/AsetController.php
@@ -9,7 +9,43 @@ class AsetController extends Controller
 {
     public function index()
     {
-        $asets = Aset::with('kategori')->get();
-        return view('aset.index', compact('asets'));
+        $data = Aset::latest()->get();
+        return view('aset.index', compact('data'));
+    }
+
+    public function create()
+    {
+        return view('aset.create');
+    }
+
+    public function store(Request $request)
+    {
+        Aset::create($request->except('_token'));
+        return redirect()->route('aset.index')->with('sukses', 'Data berhasil ditambahkan');
+    }
+
+    public function show($id)
+    {
+        $data = Aset::findOrFail($id);
+        return view('aset.show', compact('data'));
+    }
+
+    public function edit($id)
+    {
+        $data = Aset::findOrFail($id);
+        return view('aset.edit', compact('data'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $data = Aset::findOrFail($id);
+        $data->update($request->except(['_token', '_method']));
+        return redirect()->route('aset.index')->with('sukses', 'Data berhasil diupdate');
+    }
+
+    public function destroy($id)
+    {
+        Aset::destroy($id);
+        return redirect()->route('aset.index')->with('sukses', 'Data berhasil dihapus');
     }
 }
