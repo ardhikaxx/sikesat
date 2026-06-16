830c11e67c4df8c71f5e24680d876d365cbfdcc7
ardhikaxx <ardhikayanuar58@gmail.com>
2026-06-16 18:43:40 +0700

    feat: penambahan fitur resources/views/unit-pelayanan/show.blade.php

---
+++ resources/views/unit-pelayanan/show.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+@section('title', 'Detail UnitPelayanan - SIKESAT')
+@section('content')
+<div class="page-header">
+    <div class="page-header__left">
+        <h1 class="page-header__title">Detail UnitPelayanan</h1>
+        <a href="{{ route('unit-pelayanan.index') }}" class="text-decoration-none">&larr; Kembali</a>
+    </div>
+</div>
+<div class="card border-0 shadow-sm rounded-3">
+    <div class="card-body p-4">
+        <table class="table table-bordered">
+            <tr><th width="25%">Id</th><td>{{ $data->id }}</td></tr>
+            <tr><th width="25%">Kode</th><td>{{ $data->kode }}</td></tr>
+            <tr><th width="25%">Nama</th><td>{{ $data->nama }}</td></tr>
+            <tr><th width="25%">Jenis</th><td>{{ $data->jenis }}</td></tr>
+            <tr><th width="25%">Kepala Unit</th><td>{{ $data->kepala_unit }}</td></tr>
+            <tr><th width="25%">Is Aktif</th><td>{{ $data->is_aktif }}</td></tr>
+            <tr><th width="25%">Created At</th><td>{{ $data->created_at }}</td></tr>
+            <tr><th width="25%">Updated At</th><td>{{ $data->updated_at }}</td></tr>
+        </table>
+    </div>
+</div>
+@endsection
