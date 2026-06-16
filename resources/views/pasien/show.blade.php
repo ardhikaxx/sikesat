587847f4092fe01b268f1ee61bc1d5e428b4e44e
ardhikaxx <ardhikayanuar58@gmail.com>
2026-06-16 18:43:29 +0700

    feat: penambahan fitur resources/views/pasien/show.blade.php

---
+++ resources/views/pasien/show.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+@section('title', 'Detail Pasien - SIKESAT')
+@section('content')
+<div class="page-header">
+    <div class="page-header__left">
+        <h1 class="page-header__title">Detail Pasien</h1>
+        <a href="{{ route('pasien.index') }}" class="text-decoration-none">&larr; Kembali</a>
+    </div>
+</div>
+<div class="card border-0 shadow-sm rounded-3">
+    <div class="card-body p-4">
+        <table class="table table-bordered">
+            <tr><th width="25%">Id</th><td>{{ $data->id }}</td></tr>
+            <tr><th width="25%">No Rm</th><td>{{ $data->no_rm }}</td></tr>
+            <tr><th width="25%">Nik</th><td>{{ $data->nik }}</td></tr>
+            <tr><th width="25%">Nama</th><td>{{ $data->nama }}</td></tr>
+            <tr><th width="25%">Jenis Kelamin</th><td>{{ $data->jenis_kelamin }}</td></tr>
+            <tr><th width="25%">Tanggal Lahir</th><td>{{ $data->tanggal_lahir }}</td></tr>
+            <tr><th width="25%">Alamat</th><td>{{ $data->alamat }}</td></tr>
+            <tr><th width="25%">No Telepon</th><td>{{ $data->no_telepon }}</td></tr>
+            <tr><th width="25%">Jenis Pasien</th><td>{{ $data->jenis_pasien }}</td></tr>
+            <tr><th width="25%">No Bpjs</th><td>{{ $data->no_bpjs }}</td></tr>
+            <tr><th width="25%">Faskes Tingkat Satu</th><td>{{ $data->faskes_tingkat_satu }}</td></tr>
+            <tr><th width="25%">Created At</th><td>{{ $data->created_at }}</td></tr>
+            <tr><th width="25%">Updated At</th><td>{{ $data->updated_at }}</td></tr>
+        </table>
+    </div>
+</div>
+@endsection
