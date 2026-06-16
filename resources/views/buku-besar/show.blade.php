1e54776420793c4d6247e74ede1f031bd92dd9fd
ardhikaxx <ardhikayanuar58@gmail.com>
2026-06-16 18:43:20 +0700

    feat: penambahan fitur resources/views/buku-besar/show.blade.php

---
+++ resources/views/buku-besar/show.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+@section('title', 'Detail BukuBesar - SIKESAT')
+@section('content')
+<div class="page-header">
+    <div class="page-header__left">
+        <h1 class="page-header__title">Detail BukuBesar</h1>
+        <a href="{{ route('buku-besar.index') }}" class="text-decoration-none">&larr; Kembali</a>
+    </div>
+</div>
+<div class="card border-0 shadow-sm rounded-3">
+    <div class="card-body p-4">
+        <table class="table table-bordered">
+            <tr><th width="25%">Id</th><td>{{ $data->id }}</td></tr>
+            <tr><th width="25%">Akun Id</th><td>{{ $data->akun_id }}</td></tr>
+            <tr><th width="25%">Jurnal Detail Id</th><td>{{ $data->jurnal_detail_id }}</td></tr>
+            <tr><th width="25%">Tanggal</th><td>{{ $data->tanggal }}</td></tr>
+            <tr><th width="25%">No Jurnal</th><td>{{ $data->no_jurnal }}</td></tr>
+            <tr><th width="25%">Keterangan</th><td>{{ $data->keterangan }}</td></tr>
+            <tr><th width="25%">Debit</th><td>{{ $data->debit }}</td></tr>
+            <tr><th width="25%">Kredit</th><td>{{ $data->kredit }}</td></tr>
+            <tr><th width="25%">Saldo</th><td>{{ $data->saldo }}</td></tr>
+            <tr><th width="25%">Created At</th><td>{{ $data->created_at }}</td></tr>
+            <tr><th width="25%">Updated At</th><td>{{ $data->updated_at }}</td></tr>
+        </table>
+    </div>
+</div>
+@endsection
